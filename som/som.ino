59ef19becff92ccf5ab4d377ed7a1cfab3ade719
Song <217.Songs.SH@gmail.com>
2021-09-01 20:08:35 +0900

    som_flow, flow에 따른 func

--- som/som.ino
+++ som/som.ino
@@ -13,58 +13,82 @@
   CLK connects to pin 11
   CS connects to pin 10
 */
+enum som_flow {SET_TIME, TIMER, END};
+
 LedControl lc = LedControl(12, 11, 10, 1);
 
 // delay time between faces
 unsigned long delaytime = 1000;
 
-int Cat[8] = {B10001000, B11111000, B10101000, B01110001, B00100001, B01111001, B01111101, B10111110 };
-byte Apple [8] = {B00011000, B00001000, B01110110, B11111111, B11111111, B11111111, B01111010, B00110100};
-
 const int buttonPin = 6;
 
-int buttonState = 0;         // variable for reading the pushbutton status
 
+som_flow flow = SET_TIME;
 
-void setup() 
+void setup()
 {
   Serial.begin(115200);
   lc.shutdown(0, false);
   lc.setIntensity(0, 0);
   lc.clearDisplay(0);
-  
+
   pinMode(buttonPin, INPUT);
 
 }
 
+void set_time()
+{
+    int val = analogRead(A0);
+    val = map(val, 0, 1023, -1, 17) * 4;
+  
+    //  Serial.println(val);
+    for (int r = 0; r < 8; r++)
+    {
+      for (int c = 0; c < 8; c++)
+      {
+        if (r * 8 + c > val)
+        {
+          lc.setLed(0, r, c, LOW);
+        }
+        else
+        {
+          lc.setLed(0, r, c, HIGH);
+        }
+      }
+    }
+}
+
+void down_time()
+{
+    Serial.println("TIMER");
+}
+
+void end_time()
+{
+    Serial.println("END");
+}
 
 
 void loop()
 {
-
-    buttonState = digitalRead(buttonPin);
-    Serial.println(buttonState);
-
-  int val = analogRead(A0);
-  val = map(val, 0, 1023, -1, 17) * 4;
-
-  //  Serial.println(val);
-  for (int r = 0; r < 8; r++) 
+  if (flow == SET_TIME)
   {
-    for (int c = 0; c < 8; c++) 
-    {
-
-
-      if (r * 8 + c > val) 
-      {
-        lc.setLed(0, r, c, LOW);
-      }
-      else 
-      {
-        lc.setLed(0, r, c, HIGH);
-      } 
-    }
+      set_time();
   }
+  else if (flow == TIMER)
+  {
+      down_time();
+  }
+  else if (flow == END)
+  {
+      end_time();
+  }
+  
+  int buttonState = digitalRead(buttonPin);
 
 
+  if (buttonState == 0)
+  {
+      flow = TIMER;
+  }
 }
